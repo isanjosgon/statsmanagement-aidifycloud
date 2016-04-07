44d1eaaa9fffa2a3dc39e0c774c641272c77a9ea
Adriano90 <elamodeltinto@hotmail.com>
2016-04-07 23:29:58 +0200

    Added apigateway cache invalidation

--- messagebroker/broker.js
+++ messagebroker/broker.js
@@ -8,20 +8,25 @@ const redis = require('redis');
 class Broker
 {
   constructor (config,logger,setstat) {
-    if (config.host) {
-      this.client = redis.createClient(config.port,config.host);
-      this.client.auth(config.auth);
-    } else {
-      this.client = redis.createClient();
-    }
-    this.client.subscribe('SERVICE:STATSMANAGEMENT');
-    this.client.on('message',function (channel,message) {
+    this.client_sub = redis.createClient(config.port,config.host);
+    this.client_sub.auth(config.auth);
+    this.client_pub = redis.createClient(config.port,config.host);
+    this.client_pub.auth(config.auth);
+	
+    this.client_sub.subscribe('SERVICE:STATSMANAGEMENT');
+	let self = this;
+    this.client_sub.on('message',function (channel,message) {
       let service = channel.split(':')[1];
       if (service == 'STATSMANAGEMENT') {
         if (logger) {
           logger.log('request MESSAGE : ' + message);
         }
-        setstat.execute(JSON.parse(message));
+		let params = JSON.parse(message);
+        setstat.execute(params);
+		
+		let action = "CLEAN_CACHE";
+		let cleanCacheMessage = `${action}:${params.user}`;
+		self.client_pub.publish('SERVICE:APIGATEWAY', cleanCacheMessage);
       }
     });
   }
